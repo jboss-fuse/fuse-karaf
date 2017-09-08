9f26beba273bd7e50a89d7b3f519d30b1d87969b
Grzegorz Grzybek <gr.grzybek@gmail.com>
2017-09-08 11:57:25 +0200

    [ENTESB-7224] Working integration test with perfect logging

--- itests/fuse-itests-paxexam/src/test/java/org/jboss/fuse/itests/karaf/MinimalDistroIntegrationTest.java
+++ itests/fuse-itests-paxexam/src/test/java/org/jboss/fuse/itests/karaf/MinimalDistroIntegrationTest.java
@@ -15,6 +15,90 @@
  */
 package org.jboss.fuse.itests.karaf;
 
+import java.io.File;
+import javax.inject.Inject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.options.MavenUrlReference;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
 public class MinimalDistroIntegrationTest {
+
+    public static Logger LOG = LoggerFactory.getLogger(MinimalDistroIntegrationTest.class);
+
+    // @Inject is handled by org.ops4j.pax.exam.inject.internal.ServiceInjector.injectField()
+    @Inject
+    private BundleContext context;
+
+    @Rule
+    public TestName testName = new TestName();
+
+    @Before
+    public void beforeEach() {
+        LOG.info("========== Running {}.{}() ==========", getClass().getName(), testName.getMethodName());
+    }
+
+    @After
+    public void afterEach() {
+        LOG.info("========== Finished {}.{}() ==========", getClass().getName(), testName.getMethodName());
+    }
+
+    protected Option[] baseConfiguration() {
+        MavenUrlReference fuseMinimalDistro = maven()
+                .groupId("org.jboss.fuse").artifactId("jboss-fuse-karaf-minimal")
+                .type("zip").versionAsInProject();
+        MavenUrlReference karafStandardFeature = maven()
+                .groupId("org.jboss.fuse").artifactId("fuse-karaf-framework")
+                .type("xml").classifier("features").versionAsInProject();
+
+        return combine(
+                new Option[] {
+                        karafDistributionConfiguration().frameworkUrl(fuseMinimalDistro)
+                                .unpackDirectory(new File("target/paxexam")),
+                        keepRuntimeFolder(),
+                        configureConsole().ignoreLocalConsole(),
+                        editConfigurationFilePut("etc/branding.properties", "welcome", ""),
+                        editConfigurationFilePut("etc/branding-ssh.properties", "welcome", ""),
+
+                        // feature exam/4.11.0 uses:
+                        // <bundle dependency="true">mvn:org.apache.geronimo.specs/geronimo-atinject_1.0_spec/1.0</bundle>
+                        // we'll override it
+                        mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.javax-inject").versionAsInProject()
+                },
+
+                // logging configuration
+                editConfigurationFilePut("etc/org.ops4j.pax.logging.cfg", new File("src/test/resources/etc/org.ops4j.pax.logging.cfg"))
+        );
+    }
+
+    @Configuration
+    public Option[] configuration() {
+        return combine(baseConfiguration());
+    }
+
+    @Test
+    public void runMinimalFuse7DistroIntegrationTest() {
+        LOG.info("Fuse 7 Minimal is running");
+        LOG.debug("Test bundle symbolic name {}/{}", context.getBundle().getSymbolicName(), context.getBundle().getVersion());
+    }
 
 }
