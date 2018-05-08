9bc46ab122b1bb805aca5837c96ceb3d5cf05d97
Grzegorz Grzybek <gr.grzybek@gmail.com>
2018-05-08 19:06:08 +0200

    [persistence quickstarts][ENTESB-8103] Add camel-xa quickstart based on example from Viliam Kasala

---
+++ quickstarts/persistence/camel-xa/src/main/java/org/jboss/fuse/quickstarts/beginner/camel/xa/DbMessage.java
@@ -0,0 +1,56 @@
+/*
+ *  Copyright 2005-2018 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package org.jboss.fuse.quickstarts.beginner.camel.xa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity(name = "DbMessage")
+@Table(name = DbMessage.TABLENAME)
+public class DbMessage implements Serializable {
+
+    public static final String TABLENAME = "message";
+    private static final long serialVersionUID = 7017085711761726297L;
+
+    @Id
+    @Column(name = "messageText", length = 255)
+    private String messageText;
+
+    public DbMessage() {
+    }
+
+    public DbMessage(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    @Override
+    public String toString() {
+        return messageText;
+    }
+
+}
