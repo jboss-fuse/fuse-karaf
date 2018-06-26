556be92dd356ec433ea2f9cc66d0b0a5a5f83709
Grzegorz Grzybek <gr.grzybek@gmail.com>
2018-06-26 07:33:27 +0200

    [ENTESB-8042][ENTESB-8079] Add quickstart using CXF and separate server engine (Undertow)

---
+++ quickstarts/security/keycloak/keycloak-cxf/src/main/java/org/jboss/fuse/quickstarts/security/keycloak/cxf/JaxRsService.java
@@ -0,0 +1,50 @@
+/**
+ *  Copyright 2005-2017 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package org.jboss.fuse.quickstarts.security.keycloak.cxf;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This Java class with be hosted in the URI path defined by the @Path annotation. @Path annotations on the methods
+ * of this class always refer to a path relative to the path defined at the class level.
+ * <p/>
+ * For example, with 'http://localhost:8181/cxf' as the default CXF servlet path and '/crm' as the JAX-RS server path,
+ * this class will be hosted in 'http://localhost:8181/cxf/crm/customerservice'.  An @Path("/customers") annotation on
+ * one of the methods would result in 'http://localhost:8181/cxf/crm/customerservice/customers'.
+ */
+@Path("/service")
+public class JaxRsService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JaxRsService.class);
+
+    @GET
+    @Path("/hello/{hello}")
+    @Produces("application/json")
+    public Map<String, String> hello(@PathParam("hello") String hello) {
+        Map<String, String> res = new HashMap<>();
+        res.put("result", "[" + hello + "]");
+        return res;
+    }
+
+}
